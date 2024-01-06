281c60b7a2e639b01b21eb1a95d97b313aefddd6
Emmanuel Onyeozu <104470064+eminovaden@users.noreply.github.com>
2024-01-06 07:48:20 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,45 +1,97 @@
-#include "monty.h"
-fvar_t fuse = {NULL, NULL, NULL, 0};
-/**
-* main - monty code interpreter
-* @ac: number of arguments
-* @av: monty file location
-* Return: 0 on success
-*/
-int main(int ac, char *av[])
-{
-	char *fcontent;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+#ifndef MONTY_H
+#define MONTY_H
 
-	if (ac != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(av[1], "r");
-	fuse.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		fcontent = NULL;
-		read_line = getline(&fcontent, &size, file);
-		fuse.fcontent = fcontent;
-		counter++;
-		if (read_line > 0)
-		{
-			fexe(fcontent, &stack, counter, file);
-		}
-		free(fcontent);
-	}
-	free_fstack(stack);
-	fclose(file);
-return (0);
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+#define INITIAL_BUFFER_SIZE 1024
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct fvar_s - variables -args, file, line fcontent
+ * @ag: value
+ * @file: pointer to monty file
+ * @fcontent: line fcontent
+ * @fque: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+typedef struct fvar_s
+{
+	char *ag;
+	FILE *file;
+	char *fcontent;
+	int fque;
+}  fvar_t;
+
+extern fvar_t fuse;
+
+int fexe(char *, stack_t **, unsigned int, FILE *);
+void free_fstack(stack_t *);
+/*char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);*/
+/*ssize_t getstdin(char **lineptr, int file);*/
+/*char  *clean_line(char *content);*/
+/*ssize_t _getline(char **lineptr, size_t *n, FILE *stream);*/
+
+/* basic operations */
+void s_push(stack_t **, unsigned int);
+void s_pall(stack_t **, unsigned int);
+void s_pint(stack_t **, unsigned int);
+void s_pop(stack_t **, unsigned int);
+void s_swap(stack_t **, unsigned int);
+
+/* maths operations */
+void s_add(stack_t **, unsigned int);
+void s_sub(stack_t **, unsigned int);
+void s_div(stack_t **, unsigned int);
+void s_mul(stack_t **, unsigned int);
+void s_mod(stack_t **, unsigned int);
+
+/* for other operations*/
+void s_nop(stack_t **, unsigned int);
+void s_pchar(stack_t **, unsigned int);
+void s_pstr(stack_t **, unsigned int);
+void s_rotl(stack_t **, unsigned int);
+void s_rotr(stack_t **, __attribute__((unused)) unsigned int);
+
+/* for nodes*/
+void faddnode(stack_t **, int);
+void s_stack(stack_t **, unsigned int);
+void s_queue(stack_t **, unsigned int);
+void faddqueue(stack_t **, int);
+
+#endif /* MONTY_H */
